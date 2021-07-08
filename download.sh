9d854d42d55a998524e01bfc60f9e96e48abc8a9
Hendra Manudinata <aku.hendramanu@gmail.com>
2021-07-08 07:07:01 +0700

    Add missing -v argument

--- download.sh
+++ download.sh
@@ -4,5 +4,5 @@
 latestfirm=$(samloader -m $DEVICE_MODEL -r $DEVICE_REGION checkupdate)
 
 # Download and decrypt firmware
-samloader -m $DEVICE_MODEL -r $DEVICE_REGION download -D -O .
+samloader -m $DEVICE_MODEL -r $DEVICE_REGION download -v $latestfirm -D -O .
 
