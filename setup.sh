cb3a88653f93de2d78e86371b0e8534d037be3d7
Hendra Manudinata <aku.hendramanu@gmail.com>
2021-07-08 07:06:20 +0700

    Use sudo for packages process

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Update packages list
-apt-get update -y
+sudo apt-get update -y
 
 # Install required packages
-apt-get install python3 curl -y
+sudo apt-get install python3 curl -y
 
 # Install wheel, to avoid 'using legacy installer' warning
 pip3 install wheel
